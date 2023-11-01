5a17ac82a8a02c65b6dd9821c1a485d220244cba
vikrant <notvikrant80@gmail.com>
2023-11-01 10:29:48 +0530

    schema validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
 const User = require("../model/User");
+const { registerValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //check if user already exists
+  const emailExists = await User.findOne({ email: req.body.email });
+  if (emailExists) return res.status(400).send("Email already exists.");
+
+  //new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
