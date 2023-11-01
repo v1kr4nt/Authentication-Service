1d20652f121a463441369c93454ca01fbbd9cee5
vikrant <notvikrant80@gmail.com>
2023-11-01 10:38:20 +0530

    password hashing

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../model/User");
+const bcrypt = require("bcryptjs");
 const { registerValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
@@ -10,15 +11,19 @@ router.post("/register", async (req, res) => {
   const emailExists = await User.findOne({ email: req.body.email });
   if (emailExists) return res.status(400).send("Email already exists.");
 
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
   //new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
   try {
     const savedUser = await user.save();
-    res.status(201).send(savedUser);
+    res.status(201).send({ user: savedUser._id });
   } catch (err) {
     res.status(400).send(err);
   }
